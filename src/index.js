07ea9f4565749c325d7e6efbbce6b1f5234b2650
vishnuprakash004 <166406526+vishnuprakash004@users.noreply.github.com>
2025-05-13 11:54:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Footer from './components/Footer';
 import Education from './components/Education';
 import About from './components/About';
 import Contact from './components/Contact';
